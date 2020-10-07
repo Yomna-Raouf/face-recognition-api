2b235bb33ccfcbdf6d3c433c787f4c160b73e469
yomna raouf <yomnaraouf99@gmail.com>
2020-10-06 23:41:15 -0700

    image  end point  done

--- server.js
+++ server.js
@@ -107,18 +107,14 @@ app.get("/profile/:id", (req, res) => {
 
 app.put("/image", (req, res) => {
   const { id } = req.body;
-  let found = false;
-  database.users.map((user) => {
-    if (user.id === id) {
-      found = true;
-      user.entries++;
-      res.json(user.entries);
-    } /* else {
-      res.status(404).json("no such user");
-    } */
-  });
-
-  if (!found) res.status(400).json("not found");
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => {
+      res.json(entries[0]);
+    })
+    .catch((err) => res.status(400).json("unable to get entries"));
 });
 
 /* // Load hash from your password DB.
