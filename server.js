4550fc56d0e96290cb178014ccba88e00d81231b
yomna raouf <yomnaraouf99@gmail.com>
2020-10-09 03:40:54 -0700

    change DB

--- server.js
+++ server.js
@@ -24,10 +24,8 @@ app.use(bodeyParser.json());
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
-    user: `${process.env.DB_USER}`,
-    password: `${process.env.DB_PASSWORD}`,
-    database: `${process.env.DB}`,
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
   },
 });
 
